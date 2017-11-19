a0c5eb5b451c6c91a3810dc61ca1a1cb39933085
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-11-19 12:06:05 -0300

    internet permission

--- app/src/main/java/com/wedevol/fcmtest/MainActivity.java
+++ app/src/main/java/com/wedevol/fcmtest/MainActivity.java
@@ -21,7 +21,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements IRequestListener {
 
     private static final String TAG = "MainActivity";
-    public static final String FCM_PROJECT_SENDER_ID = "972853060639";
+    public static final String FCM_PROJECT_SENDER_ID = "431269160141";
     public static final String FCM_SERVER_CONNECTION = "@gcm.googleapis.com";
     public static final String BACKEND_ACTION_MESSAGE = "MESSAGE";
     public static final String BACKEND_ACTION_ECHO = "ECHO";
